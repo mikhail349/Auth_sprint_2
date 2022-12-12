74255c36588c8925809cbd2b83a376679fb5e16d
mikhail349 <mikhail349349@gmail.com>
2022-12-13 00:14:15 +0300

    refactor oauth

--- src/api/v1/oauth/ya.py
+++ src/api/v1/oauth/ya.py
@@ -2,9 +2,21 @@ from src.core.config import yandex_oauth2_settings as settings
 from src.api.v1.oauth.base import create_blueprint
 
 
-def get_id(data):
+def get_user_id(data):
     """Метод для получения уникального идентификатора пользователя."""
     return data["id"]
+
+
+def construct_info_request(token: str, headers: dict, params: dict):
+    """Метод для переопределения параметров запроса информации о пользователе.
+
+    Args:
+        token: токен доступа
+        headers: заголовки запроса
+        params: параметры запроса
+
+    """
+    headers["Authorization"] = "bearer {}".format(token)
 
 
 ya = create_blueprint(
@@ -16,6 +28,7 @@ ya = create_blueprint(
     token_url=settings.token_url,
     base_url=settings.base_url,
     redirect_url=settings.redirect_url,
-    get_id=get_id
+    get_user_id=get_user_id,
+    construct_info_request=construct_info_request
 )
 """Blueprint Yandex OAuth2."""
